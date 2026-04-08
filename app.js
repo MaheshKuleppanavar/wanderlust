79b46a45314fd32b7663a63b39d5eda3b630d535
Mahesh Kudleppanavar <maheshkudleppanavar@gmail.com>
2026-04-08 23:35:19 +0530

    Bug fixed

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ main()
     console.log('Connected Successfully 🔗');
     const PORT = process.env.PORT || 8080;
     app.listen(PORT, () => {
-        console.log(`Server started at http://localhost:{PORT} 🚀`);
+        console.log(`Server started at http://localhost:${PORT} 🚀`);
     });
 })
 .catch(err => {
